f848a1372d88b31c34b78592e318426c188a8a12
Borja1998 <160590659+Borja1998@users.noreply.github.com>
2024-07-25 17:27:52 +0200

    Cambios ajustados
    footer hecho falta hacer responsibe y colocar estilos

--- vite-project/src/components/footer/footer.tsx
+++ vite-project/src/components/footer/footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="relative bg-gradient-to-r from-secondaryBlue to-secondaryGreen w-[100vw] h-[60vh] flex flex-col justify-center ">
       <div className='flex flex-row w-full h-full justify-around items-start text-white'>
-        <div className='flex flex-col items-start relative top-6 right-64'>
+        <div className='flex flex-col items-start relative top-6 right-60'>
           <h1 className='text-lg'>Apartados:</h1>
           <Link to={'/'} className='text-lg'>Home</Link>
           <Link to={'/'} className='text-lg'>Protectoras</Link>
@@ -15,14 +15,14 @@ const Footer: React.FC = () => {
           <Link to={'/'} className='text-lg'>Contacto</Link>
         </div>
 
-        <div className='flex flex-col items-start relative top-6 right-84'>
+        <div className='flex flex-col items-start relative top-6 right-[40rem]'>
           <h1 className='text-lg'>Happy Paws</h1>
           <Link to={'/'} className='text-lg'>Sobre Nosotros</Link>
           <Link to={'/'} className='text-lg'>Preguntas frecuentes</Link>
         </div>
       </div>
 
-      <img src={Logo} alt="Logo" className='relative w-[5%] h-[90px] bottom-14 align-center justify-center' />
+      <img src={Logo} alt="Logo" className='relative w-[5%] h-[90px] bottom-14 left-[40rem] ' />
     </footer>
   );
 };
